e7acf686a8ea1b1f877b092045c926af3c90b906
wandeba <65151476+wandeba@users.noreply.github.com>
2025-11-07 19:32:01 +0300

    Update preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -1,1 +1,6 @@
+const { contextBridge } = require('electron');
 
+contextBridge.exposeInMainWorld('electronAPI', {
+  platform: process.platform,
+  getAppVersion: () => process.env.npm_package_version || '1.0.0'
+});
